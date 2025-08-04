6e4c79cfd8f9d8cee7580dfbff373eb4aa264b1f
Tolu Fash <fashinatoluwa@gmail.com>
2025-08-04 07:50:32 +0100

    New Changes

---
+++ js/reset.js
@@ -0,0 +1,12 @@
+// function reset() {
+//   let reset_all = document.querySelector(".form-control");
+//   reset_all.foreach((val, index) => {
+//     val.value = "";
+//   });
+// }
+
+// let btnn = document.querySelector(".btn-default");
+// btnn.addEventListener("click", (e) => {
+//   e.preventDefault();
+//   reset();
+// });
